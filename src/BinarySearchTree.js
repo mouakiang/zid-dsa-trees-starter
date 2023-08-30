c89ab98d801511713c1f3d0c6a0c389c7c38ec98
mouakiang <mouakkia@gmail.com>
2023-08-30 16:52:28 -0400

    added height of a BST

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -117,6 +117,33 @@ class BinarySearchTree {
         return values;
     }
 
+    getHeight(currentHeight = 0) {
+        // BASE CASE:
+        // If the current node doesn't have a left or right child,
+        // then the base case is reached, and the function can return the height.
+        if (!this.left && !this.right) return currentHeight;
+
+        // RECURSIVE CASE:
+        // Otherwise, compute the new height.
+        const newHeight = currentHeight + 1;
+
+        // If there's no left child, recurse down the right subtree only,
+        // passing down the height of the current node.
+        if (!this.left) return this.right.getHeight(newHeight);
+
+        // If there's no right child, recurse down the left subtree only,
+        // passing down the height of the current node.
+        if (!this.right) return this.left.getHeight(newHeight);
+
+        // If both children exist, recurse down both subtrees,
+        // passing down the height of the current node.
+        const leftHeight = this.left.getHeight(newHeight);
+        const rightHeight = this.right.getHeight(newHeight);
+
+        // Return the greater of the left or right subtree heights.
+        return Math.max(leftHeight, rightHeight);
+    }
+
     _replaceWith(node) {
         if (this.parent) {
             if (this == this.parent.left) {
