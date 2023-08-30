75cee3327e9f6db93b8ad11369bea9a11b81f196
mouakiang <mouakkia@gmail.com>
2023-08-30 17:03:07 -0400

    checking if its a BST

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -144,6 +144,20 @@ class BinarySearchTree {
         return Math.max(leftHeight, rightHeight);
     }
 
+    isBST() {
+        // Use the existing `dfsInOrder()` method to traverse the tree.
+        const values = this.dfsInOrder();
+
+        // Check if the array returned by the in-order DFS is a sorted array.
+        for (let i = 1; i < values.length; i++) {
+            // Compare the current and previous values.
+            if (values[i] < values[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     _replaceWith(node) {
         if (this.parent) {
             if (this == this.parent.left) {
